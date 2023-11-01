ab67e9cb905d4b7ea59181f971f3bfe5b4d3aa0c
charkaouizouhir <charkaouizouhir.me@gmail.com>
2023-11-01 08:46:23 +0100

    forum
    forum

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Page1 } from './components/Page1';
+import { Page2 } from './components/Page2';
+import { Page3 } from './components/Page3';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <Page1/>
+      <br></br>
+      <Page2/>
+      <br></br>
+      <Page3/>
+    </div>
+  );
+}
+
+export default App;
